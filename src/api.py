a6086ec049bc040001c57e2852468c57e5bace92
alina <alinakrylova2004@mail.ru>
2025-06-17 18:31:56 +0300

    Исправления

--- src/api.py
+++ src/api.py
@@ -2,35 +2,48 @@ from abc import ABC, abstractmethod
 import requests
 from typing import List, Dict
 
+from requests import Response
+
 
 class VacancyAPI(ABC):
     @abstractmethod
-    def get_vacancies(self, search_query: str) -> List[Dict]:
+    def get_vacancies(self) -> list[dict]:
         """Метод для получения вакансий по поисковому запросу"""
         pass
 
 
 class HeadHunterAPI(VacancyAPI):
-    def __init__(self):
-        self._base_url = "https://api.hh.ru/vacancies"
 
-    def _connect(self) -> None:
-        """Приватный метод для проверки подключения к API"""
-        response = requests.get(self._base_url)
-        if response.status_code != 200:
-            raise ConnectionError("Не удалось подключиться к API hh.ru")
+    LIST_ID = [
+        11669695,
+        2066667,
+        1911403,
+        4685961,
+        197566,
+        2300703,
+        1189354,
+        45124,
+        9498120,
+        2393,
+        4437201,
+    ]
 
-    def get_vacancies(self, search_query: str) -> List[Dict]:
-        """Получение вакансий с hh.ru по поисковому запросу"""
-        self._connect()
-        params = {
-            'text': search_query,
-            'area': 113,  # 113 - Россия
-            'per_page': 10
-        }
-        response = requests.get(self._base_url, params=params)
-        if response.status_code == 200:
-            return response.json().get('items', [])
-        else:
-            return []
+    def __init__(self) -> None:
+        """Метод для инициализации экземпляра класса"""
+        self.__params: dict = {"text": "", "page": 0, "per_page": 100, "employer_id": 0, "area": 113}
+        self.__vacancies: list = []
 
+    def get_vacancies(self) -> list[dict]:
+        """Метод для получения вакансий по поисковому запросу"""
+
+        for i in self.LIST_ID:
+            self.__params["employer_id"] = i
+
+            response: Response = requests.get("https://api.hh.ru/vacancies",
+                                    headers={"User-Agent": "HH-User-Agent"},
+                                    params=self.__params)
+            if response.status_code == 200:
+                vacancies = response.json()["items"]
+                self.__vacancies.extend(vacancies)
+
+        return self.__vacancies
--- src/vacancy.py
+++ src/vacancy.py
@@ -4,10 +4,20 @@ import f
 
 
 class Vacancy:
-    __slots__ = ('title', 'url', 'salary', 'description')
+    __slots__ = ('title', 'url', 'salary', 'description', 'vacancy_id', 'company_id', 'company_name',)
 
-    def __init__(self, title: str, url: str, salary: Optional[str], description: str):
+    def __init__(self, title: str,
+                 url: str,
+                 salary: Optional[str],
+                 vacancy_id: str,
+                 company_id: str,
+                 company_name: str,
+                 description: str):
         self.title = title
+        self.vacancy_id = vacancy_id
+        self.company_id = company_id
+        self.company_name = company_name
+
         self.url = url
         self.salary = self._validate_salary(salary)
         self.description = description
@@ -47,5 +57,8 @@ class Vacancy:
             alternate_url = f.ichain(item, 'alternate_url') or 'Нет url'
             salary = f.ichain(item, 'salary', 'from') or 0
             requirement = f.ichain(item,'snippet', 'requirement') or 'Нет описания'
-            list_vacancies.append(Vacancy(name, alternate_url, salary, requirement))
+            vacancy_id = f.ichain(item,'id') or 0
+            company_id = f.ichain(item,'employer', 'id') or 'Нет ИД'
+            company_name = f.ichain(item,'employer', 'name') or 'Нет названия'
+            list_vacancies.append(Vacancy(name, alternate_url, salary, vacancy_id, company_id, company_name, requirement))
         return list_vacancies
